d8a771dcfe5fd6cfa4e00a29bf06396b662a1dc0
chLi <565353780@qq.com>
2024-05-07 18:40:12 +0800

    update setup bash files

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,2 +1,7 @@
-cmake -Bbuild . -DCMAKE_BUILD_TYPE=Release
+cd ../sibr-core
+
+rm -rf build
+rm -rf install
+
+cmake -Bbuild . -DCMAKE_BUILD_TYPE=Release -G Ninja
 cmake --build build -j --target install
