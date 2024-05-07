98ba33989397f0e1460b55f3352277255aebbd60
chLi <565353780@qq.com>
2024-05-07 18:45:45 +0800

    add related apt packages install

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+sudo apt install -y libglew-dev libassimp-dev libboost-all-dev libgtk-3-dev libopencv-dev \
+	libglfw3-dev libavdevice-dev libavcodec-dev libeigen3-dev libxxf86vm-dev libembree-dev -y
+
 cd ../sibr-core
 
 rm -rf build
