551ed9127c0aac1cededc6989da20f58e7b80138
chLi <565353780@qq.com>
2025-12-17 13:37:20 +0800

    re-order setup bash files

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,10 +1,15 @@
-sudo apt install -y libglew-dev libassimp-dev libboost-all-dev libgtk-3-dev libopencv-dev \
-  libglfw3-dev libavdevice-dev libavcodec-dev libeigen3-dev libxxf86vm-dev libembree-dev -y
+sudo apt install -y libglew-dev libassimp-dev \
+  libboost-all-dev libgtk-3-dev libopencv-dev \
+  libglfw3-dev libavdevice-dev libavcodec-dev \
+  libeigen3-dev libxxf86vm-dev libembree-dev
 
 rm -rf build
 rm -rf install
 
 pip install ninja
 
-cmake -Bbuild . -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -Bbuild . \
+  -DCMAKE_BUILD_TYPE=Release \
+  -G Ninja \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build -j --target install
